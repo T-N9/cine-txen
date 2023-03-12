0bd975f6cade434d19fa2109440e65d34306065c
T-N9 <thethoriumtension@gmail.com>
2023-03-12 18:18:08 +0630

    [Nav] readded

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,12 @@ import "../styles/font.css";
 import "../styles/globals.css";
 import "../styles/style.css";
 
+import NavBar from "../components/NavBar";
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <main>
+      <NavBar/>
       <Component {...pageProps} />
     </main>
   );
