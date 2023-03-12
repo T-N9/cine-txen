76b8e31a2db12f040af2acd90c5ae95a1bcfe690
T-N9 <thethoriumtension@gmail.com>
2023-03-12 23:14:37 +0630

    [Nav] logo added

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 const NavBar = () => {
   return (
     <nav>
       <h1>THis is a NavBar.</h1>
+      <Image
+        src={"/assets/cine-logo.png"}
+        alt="CINE logo"
+        width={153}
+        height={62}
+        className="cursor-pointer"
+      />
     </nav>
   );
 };
