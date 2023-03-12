845def0e6aa09a06ff96702c6a979069e2f30a38
T-N9 <thethoriumtension@gmail.com>
2023-03-12 17:54:19 +0630

    [NavBar] removed

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,9 @@ import "../styles/font.css";
 import "../styles/globals.css";
 import "../styles/style.css";
 
-import NavBar from "../components/NavBar";
-
 const MyApp = ({ Component, pageProps }) => {
   return (
     <main>
-      <NavBar />
       <Component {...pageProps} />
     </main>
   );
