366d70929580f6961395876ae7dd7f52991cc96e
T-N9 <thethoriumtension@gmail.com>
2023-03-13 07:05:22 +0630

    [Nav] test

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,31 @@
 import Image from "next/image";
 
+import Link from "next/link";
+
+import { useState } from "react";
+
+/* Images & icons */
+import { MenuRounded, CloseRounded } from "@mui/icons-material";
+
+/* UI Data */
+import { navItems } from "../constants/uiData";
+
+import { useRouter } from "next/router";
+import * as routes from "../constants/routePaths";
+
 const NavBar = () => {
   return (
     <nav>
       <h1>THis is a NavBar.</h1>
-      <Image
-        src={"/assets/cine-logo.png"}
-        alt="CINE logo"
-        width={153}
-        height={62}
-        className="cursor-pointer"
-      />
+      <Link href='/'>
+        <Image
+          src={"/assets/cine-logo.png"}
+          alt="CINE logo"
+          width={153}
+          height={62}
+          className="cursor-pointer"
+        />
+      </Link>
     </nav>
   );
 };
